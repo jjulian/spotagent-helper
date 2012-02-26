b3e4b27a473c2a4a4d57113edd1fa63add139c99
Jonathan Julian <jonathan.m.julian@gmail.com>
2012-02-25 19:20:51 -0500

    Communicate the threat level to the host page. WIP

---
+++ threat_injection.js
@@ -0,0 +1,6 @@
+var communication = document.getElementById('spotagent-address');
+if (communication) {
+  var address = unescape(communication.getAttribute('data-address'));
+  console.log("OK, looking for "+address);
+  //TODO
+}
